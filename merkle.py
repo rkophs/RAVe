99c928cee552404939d86427a67e0b329f875636
Ryan Kophs <rkophs@gmail.com>
2016-04-12 12:59:57 -0400

    code cleanup

--- merkle.py
+++ merkle.py
@@ -9,32 +9,30 @@ from Crypto.Cipher import AES
 
 class Cipher:
 
-	BS = AES.block_size
-
 	@staticmethod 
-	def pad( s ):
-		return s + (Cipher.BS - len(s) % Cipher.BS) * chr(Cipher.BS - len(s) % Cipher.BS)
+	def pad(s):
+		return s + (16 - len(s) % 16) * chr(16 - len(s) % 16)
 
 	@staticmethod
-	def unpad( s ):
+	def unpad(s):
 		return s[0:-ord(s[-1])]
 
 	@staticmethod
-	def _int64_to_bytes(self, x):
+	def _int64_to_bytes(x):
 		result = chr((x >> 56) & 0xFF) + chr((x >> 48) & 0xFF)
 		result += chr((x >> 40) & 0xFF) + chr((x >> 32) & 0xFF)
 		result += chr((x >> 24) & 0xFF) + chr((x >> 16) & 0xFF)
 		result += chr((x >> 8) & 0xFF) + chr(x & 0xFF)
 		return result
-		
+
 	def __init__(self, enc_key, nonce):
 		self.enc_key = enc_key
 		self.cnter_cb_called = 0 
 		self.secret = nonce
 
-	def _counter_callback( self ):
+	def _counter_callback(self):
 		self.cnter_cb_called += 1
-		return self.secret + self._int64_to_bytes(self.cnter_cb_called)
+		return self.secret + Cipher._int64_to_bytes(self.cnter_cb_called)
 
 	def tag(self ):
 		return self.enc_tag
@@ -101,20 +99,22 @@ class Verifier:
 		self.signer = Signer(auth_key)
 		self.cipher = Cipher(enc_key, enc_nonce)
 
-	def verify(self, ciphertext, enc_tag, mac):
-		self.cipher.decryptTokens((len(ciphertext) // 16), enc_tag)
-		return self._validMac(ciphertext, enc_tag, mac) and self._timely() and self._validMsgLength()
-
 	def _validMac(self, ciphertext, enc_tag, mac):
 		return self.signer.mac(ciphertext, enc_tag) == mac
 
 	def _timely(self):
 		return self.cipher.expiration() > time.time()
 
-	def _validMsgLength(self):
+	def _validMsgLength(self, ciphertext):
 		return self.cipher.msgLen() == len(ciphertext)
 
+	def verify(self, ciphertext, enc_tag, mac):
+		self.cipher.decryptTokens((len(ciphertext) // 16), enc_tag)
+		return self._validMac(ciphertext, enc_tag, mac) and self._timely() and self._validMsgLength(ciphertext)
+
 def main():
+	msg = "0123456789abcdef0123456789ABCDEF"
+	expiration = int(time.time() + 1000)
 	auth_key = "0123456789abcdef"
 	enc_key = "0123456789012345"
 	enc_nonce = "01234567"
@@ -122,7 +122,7 @@ def main():
 	enc_cipher = Cipher(enc_key, enc_nonce)
 	dec_cipher = Cipher(enc_key, enc_nonce)
 
-	ciphertext = enc_cipher.encrypt("0123456789abcdef0123456789ABCDEF", int(time.time()+1000))
+	ciphertext = enc_cipher.encrypt(msg, expiration)
 	enc_tag = enc_cipher.tag()
 
 	signer = Signer(auth_key)
@@ -133,3 +133,5 @@ def main():
 
 	print(verified)
 	print(dec_cipher.decrypt(ciphertext))
+
+main()
